a57c6d58aae8cc6e91bf58e8a5ffd1957e3be985
Hyusein <hyusein.ali.hyusein98@gmail.com>
2023-08-11 17:49:58 +0200

    Added some basic UI

--- VoiceCommandsApp/ContentView.swift
+++ VoiceCommandsApp/ContentView.swift
@@ -9,18 +9,52 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+         
+            List {
+                Section{
+                    CardView(text: "Status")
+                    CardView(text: "Current")
+                }
+                Section(header: Text("Values")) {
+                    CardView(text: "Testing")
+                    CardView(text: "Testing")
+                    CardView(text: "Testing")
+                }
+               }
+            Button("Button title") {
+                print("Button tapped!")
+            }
+            
         }
-        .padding()
+//        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct CardView: View {
+    let text: String
+
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 25, style: .continuous)
+                .fill(.white)
+//                .shadow(radius: 5)
+
+//            VStack {
+                Text(text)
+                    .font(.largeTitle)
+                    .foregroundColor(.black)
+//            }
+//            .padding(20)
+            .multilineTextAlignment(.center)
+        }
+        .frame(width: 200, height: 80)
     }
 }

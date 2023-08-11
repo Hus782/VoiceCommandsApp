acc1ca8831e7e59aedc8d7c848ab78df44b1891d
Hyusein <hyusein.ali.hyusein98@gmail.com>
2023-08-11 17:06:57 +0200

    First commit

---
+++ VoiceCommandsApp/VoiceCommandsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceCommandsAppApp.swift
+//  VoiceCommandsApp
+//
+//  Created by user240865 on 8/11/23.
+//
+
+import SwiftUI
+
+@main
+struct VoiceCommandsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
